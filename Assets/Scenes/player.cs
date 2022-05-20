4765d74537a4ccbe29572e732b2aa4bebf6d6b8d
unknown <SENA@MEDAPRCCSFSD340.FormacionSena.RED>
2022-05-20 14:50:12 -0500

    la isla player
    player0w0

--- Assets/Scenes/player.cs
+++ Assets/Scenes/player.cs
@@ -7,6 +7,7 @@ public class player : MonoBehaviour
     [Header("movimiento de personaje")]
     public float speedMovement;
     //acceso-//tipo de variable //nombre
+
     public Vector3 direccion;
     public CharacterController controller;
 
@@ -14,24 +15,68 @@ public class player : MonoBehaviour
     public Vector2 MouseMovement;
     public Camera playerCamera;
     public float rotationcameraX;
+    public float rotationcameraY;
+    public float rotationplayerY;
+    public float gravity = 9.8f;
 
-
-
-
-    //movimiento del personaje  
+    [Header("Salto Del personaje")]
+    public float JumpHeight;
+    
+   
     private void Update()
     {
+
+      //movimiento del personaje 
+        direccion.x = Input.GetAxis("Horizontal");
+        direccion.z = Input.GetAxis("Vertical");
+        direccion.y += gravity * Time.deltaTime;
+
+        if (controller.isGrounded && direccion.y >0)
+        {
+            direccion.y = -2f;
+        }
+
+        direccion = transform.TransformDirection(direccion);
+        controller.Move(direccion * Time.deltaTime * speedMovement);
+
         rotationcameraX -= MouseMovement.y;
+        rotationplayerY += MouseMovement.x;
+       
+
+        /* if (rotationcameraX > 40 )
+        {
+            rotationcameraX = 40;
+
+        }
+        if (rotationcameraX<-40)
+        {
+            rotationcameraX = -40; 
+        }*/
+
+        
+        //Rotacion limmitada
+        rotationcameraX = Mathf.Clamp(rotationcameraX, -40, 40);
+
+        //Rotacion
         MouseMovement.x = Input.GetAxis("Mouse X");
         MouseMovement.y = Input.GetAxis("Mouse Y");
-        playerCamera.transform.localRotation = Quaternion.Euler(rotationcameraX, 0, 0);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationcameraX,0, 0);
+        controller.transform.rotation = Quaternion.Euler(0, rotationplayerY, 0);
+
+            
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            direccion.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }        
+            
+        
+        
+        
         
         //vector 2= (h-v)
         //Direccion en la que se mueve el personaje
 
-        direccion.x = Input.GetAxis("Horizontal");
-        direccion.z = Input.GetAxis("Vertical");
-        controller.Move(direccion * Time.deltaTime * speedMovement);
+        
 
 
 
