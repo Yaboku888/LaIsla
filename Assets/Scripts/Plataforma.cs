e7eec9cf5e3ddb0b67f00491ed474f142bdd1729
unknown <SENA@MEDAPRCCSFSD340.FormacionSena.RED>
2022-05-26 16:07:51 -0500

    plataforma
    plataforma

---
+++ Assets/Scripts/Plataforma.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour
+{
+    public GameObject plataforma;
+    public GameObject boton;
+
+    public Transform positionmin;
+    public Transform positionmax;
+    public float speedmovent;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
