996183ea3fc7782d44239ef2cef6d4e7febc3677
unknown <SENA@MEDAPRCCSFSD340.FormacionSena.RED>
2022-06-13 13:58:33 -0500

    enemy to purshe
    naranaranara

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Player))]
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Player movement;
+    public Transform jugador;
+    public float rango = 15;
+    private float distance;
+
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        distance = Vector3.Distance(transform.position, jugador.position);
+        if (distance <= rango)
+        {
+            if (Vector3.Dot((jugador.position - transform.position).normalized, transform.TransformDirection(Vector3.forward).normalized) < 0.98f)
+            {
+                if (Vector3.Dot((jugador.position-transform.position).normalized, transform.TransformDirection(Vector3.right).normalized)> 0f)
+                {
+                    movement.Rotation(-10f);
+                }
+           
+
+            
+                
+
+            }
+            else
+            {
+                movement.Rotation(10f);
+            }
+
+            
+        }
+                movement.Move(1, 0);
+    } 
+
 }
